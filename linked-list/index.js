fd01d90ec1b749ed46563d1d4873a81338a2bf24
Judah Oyedele <oyedeleinioluwajudah@gmail.com>
2026-09-19 03:21:29 +0100

    made linkedlist into aray function

--- linked-list/index.js
+++ linked-list/index.js
@@ -30,5 +30,22 @@ const list = new LinkedList();
 
 list.insert(10);
 list.insert(20);
+list.insert(30);
 
-console.log('head:' ,list.head);
+console.log('head:', list.head);
+
+const values = makeArrayFromNode(list)
+console.log('Values: ', values)
+
+function makeArrayFromNode (list) {
+    let result = [], current = list.head
+
+    result.push(current.value)
+
+    while (current.next !== null) {
+		current = current.next
+        result.push(current.value)
+	}
+
+    return result
+}
